7bbac9c6504471a5e5f6af681f8eea8051b49650
Jack West <jackwest245@gmail.com>
2023-01-22 16:23:48 -0600

    checking in updates

--- dmd_dataset_parser.py
+++ dmd_dataset_parser.py
@@ -4,34 +4,77 @@ import numpy as np
 import torch
 import cv2
 import torchvision
+import os
+
+import matplotlib.pyplot as plt
 
 from torch.utils.data import Dataset, DataLoader
 from imutils.video import FileVideoStream
 from tqdm import tqdm
+from scipy.spatial.distance import euclidean
+from skimage.transform import resize
+
+
+
 
 class DMD(Dataset):
     #Grabs all of the files from parsed DMD dataset generated from dataset_parser.py
-    def __init__(self):
+    def __init__(self,amount=0,training=False):
+        self.use_bounding = False
+        #https://github.com/Itseez/opencv/blob/master/data/haarcascades
+        self.face_cascades = []
+        #https://github.com/Itseez/opencv/blob/master/data/haarcascades
+        self.eye_cascades = []
+        
+        for root,dirs,files in os.walk("cascades"):
+            for file in files:
+                if "xml" in file:
+                    if "face" in file:
+                        self.face_cascades.append(root+"/"+file)
+                    elif "eye" in file:
+                        self.eye_cascades.append(root+"/"+file)
+
         #Grabbing path to parsed datset.
         self.path = "../dataset_dmd/clips/"
-        
-        #Extracting all files within the folder.
         file_list = glob.glob(self.path + "*")
-        
-        #initalizing some variables
-        self.data = []
-        self.classes = []
 
-        print("LOADING DATASET")
-        #Adding all MP4s into a data array.
-        for class_path in tqdm(file_list):
+        #Retrieve all possible labels
+        self.classes = []
+        for class_path in file_list:
             class_name = class_path.split("/")[-1].split(";")[1]
             
             if class_name not in self.classes:
                 self.classes.append(class_name)
 
-            for vid_path in glob.glob(self.path+"*.mp4"):
-                self.data.append([vid_path, class_name])
+        t_index = (3*len(file_list))//4
+
+        
+        if training:
+            if amount == 0:
+                #Extracting all files within the folder.
+                file_list = glob.glob(self.path + "*")[0:t_index]
+            else:
+                #Grabbing a subset of files
+                file_list = glob.glob(self.path + "*")[0:amount]
+        else:
+            if amount == 0:
+                #Extracting all files within the folder.
+                file_list = glob.glob(self.path + "*")[t_index:]
+            else:
+                #Grabbing a subset of files
+                if t_index+amount > len(file_list):
+                    raise RuntimeError("The amount of testing files exceeds amount of data available.") 
+                else:
+                    file_list = glob.glob(self.path + "*")[t_index:t_index+amount]
+        
+        #initalizing some variables
+        self.data = []
+
+        print("LOADING DATASET")
+        #Adding all MP4s into a data array.
+        for class_path in tqdm(file_list):
+            class_name = class_path.split("/")[-1].split(";")[1]
+            self.data.append([class_path, class_name])
 
         #Creating class map
         self.class_map = {}
@@ -41,45 +84,111 @@ class DMD(Dataset):
             i+=1
         
         #Getting the dimension of the frames
-        capture2 = cv2.VideoCapture(self.data[0][0])
+        frame_size = (500,500)
 
-        frame_width = int(capture2.get(3))
-        frame_height = int(capture2.get(4))
-        frame_size = (frame_width,frame_height)
-
-        capture2.release()
-
+       
         self.img_dim = frame_size    
         
+    def create_bounding_box(self,frame,indf,inde):
+        c=0
+        img = frame.copy()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = cv2.CascadeClassifier(self.face_cascades[indf]).detectMultiScale(gray,1.3, 5)
+        roi_color = frame
+        roi_gray = gray
+        for (x,y,w,h) in faces:
+            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
+            
+            eyes = cv2.CascadeClassifier(self.eye_cascades[inde]).detectMultiScale(roi_gray)
+            if len(eyes) != 2:
+                for (ex,ey,ew,eh) in eyes:
+                    for (ex2,ey2,ew2,eh2) in eyes:
+                        if ex2 == ex and ey2 == ey:
+                            continue
+                        else:
+                            d = euclidean( (ex,ey),(ex2,ey2) )
+                            if d > 100 and d < 150:
+                                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+                                cv2.rectangle(roi_color,(ex2,ey2),(ex2+ew2,ey2+eh2),(0,255,0),2)
+                                c+=2
+            elif len(eyes) == 1:
+                for (ex,ey,ew,eh) in eyes:
+                    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+                c=1
+            else:
+                for (ex,ey,ew,eh) in eyes:
+                    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+                c=2
+        return c,img,roi_color,roi_gray
+    
     def __len__(self):
-        return len(self.data)    
+        return len(self.data)
+            
         
     def __getitem__(self, idx):
+        #Gets the video path and label
         vid_path, class_name = self.data[idx]
         class_id = self.class_map[class_name]
-
+        
+        #Opens a video stream for directly reading frames
         capture = FileVideoStream(vid_path)
 
+        #Puts frames into an array
         tmp_arr = []
         capture.start()
+        
+        oldpf = 0
+        oldpe = 0
         while capture.running():
             frame = capture.read()
+            done = False
             if type(frame) != type(None):
-                tmp_arr.append(frame)
-        capture.stop()
+                if self.use_bounding:
+                    if oldpe > 0 or oldpf > 0:
+                        c,img,rc,rg = self.create_bounding_box(frame,oldpf,oldpe)
+                        if c == 2:
+                            tmp_arr.append(img)
+                            continue
+                        
+                    
+                    for indf in range(len(self.face_cascades)):
+                        for inde in range(len(self.eye_cascades)):
+                            c,img,rc,rg = self.create_bounding_box(frame,indf,inde)
+                    
+                            if c == 2 or c == 1:
+                                print(f"SUCCESS face cascade = {indf} eye cascade = {inde} Eye Count = {c}")
+                                tmp_arr.append(img)
+                                done = True
+                                # if oldpe != inde or oldpf != indf:
+                                #     plt.imshow(img)
+                                #     plt.show()
+                                oldpf = indf
+                                oldpe = inde
+                                break
+                            
+                        
+                        if done:
+                            break
+                else:
+                    tmp_arr.append(resize(frame,self.img_dim))
+                        
 
+        capture.stop()
+        print(len(tmp_arr))
+        #Builds tensors out of the array of frames
         out = np.array(tmp_arr)
         tensors = torch.tensor(out)
-        class_id = torch.tensor([class_id])
+        class_id = torch.tensor([class_id for i in range(len(out))])
         
-        if len(tensors) == 0:
-            return None,class_id
-        else:
-            return  tensors,class_id
+        
+        return  tensors,class_id
 
 if __name__ == "__main__":
     print(torchvision.get_video_backend())
-    d = DMD()
+    d = DMD(amount=10)
     print(torch.cuda.get_device_name(0))
     
-    print(d[-1])
+    for ele in d:
+        print(ele)
---
+++ ml_model.py
@@ -0,0 +1,85 @@
+import os
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import torch.optim as optim
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class NeuralNetwork(nn.Module):
+    def __init__(self,of=10):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        #This defines the order of execution of the network
+        self.linear_relu_stack = nn.Sequential(
+            #Applies a linear transformation to input
+            #Arg 1 is the size of the input. Meaning, how many dimensions there are.
+            #Arg 2 is the output size. 
+            nn.Linear(500*500*3, 500),
+            nn.ReLU(),
+            nn.Linear(500, 500),
+            nn.ReLU(),
+            nn.Linear(500, 40),
+            
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        
+        logits = self.linear_relu_stack(x)
+        return logits
+
+if __name__ == "__main__":
+    from dmd_dataset_parser import *
+    d_training = DMD(amount=100,training=True)
+    d_testing = DMD(amount=100)
+    model = NeuralNetwork(of=len(d_training.classes)).to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    print(len(d_training.classes))
+    for epoch in range(5):  # loop over the dataset multiple times
+        model.train()
+        running_loss = 0.0
+
+        f = 0
+        for i, data in enumerate(d_training):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+            print(f"Evaluationg video {i} with {len(inputs)} frames")
+            batch_size = 20
+            j=0
+            while batch_size*j < len(inputs):
+
+                if batch_size*(j+1) > len(inputs):
+                    frame = inputs[batch_size*j:].to(torch.float32).cuda()
+                    label = labels[batch_size*j:].cuda()
+                else:
+                    frame = inputs[batch_size*j:batch_size*(j+1)].to(torch.float32).cuda()
+                    label = labels[batch_size*j:batch_size*(j+1)].cuda()
+                
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward + backward + optimize
+                outputs = model(frame)
+                # print( f"Prediction: {outputs} Label: {label}")
+                loss = criterion(outputs, label)
+                loss.backward()
+                optimizer.step()
+
+                # print statistics
+                running_loss += loss.item()
+                if f%100 == 0:                
+                    print(f'[epoch:{epoch + 1}, iter:{i :5d}, frame:{f}] loss: {running_loss/100}')
+                    running_loss = 0.0
+                f+=20
+
+                j+=1
+    
+
+
+print('Finished Training')
