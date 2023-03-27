c6083009e656e754ebc3385ae95683b98919a0ba
Jack West <jackwest245@gmail.com>
2023-03-27 10:18:30 -0500

    added code for cross validation to begin training the model.

--- ml_model.py
+++ ml_model.py
@@ -3,16 +3,14 @@ import torch
 import gc
 import torchvision
 from torch import nn
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, SubsetRandomSampler
+from sklearn.model_selection import KFold
 from torchvision import datasets, transforms
 import torch.optim as optim
 import sqlite3
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 root_path = os.environ["DMD_ROOT"]
-
-db = sqlite3.connect('dmd.db',check_same_thread=False)
-cur = db.cursor()
 
 
 class NeuralNetwork(nn.Module):
@@ -37,6 +35,23 @@ class NeuralNetwork(nn.Module):
         
         logits = self.linear_relu_stack(x)
         return logits
+    
+def get_accuracy(model, data_loader, criterion):
+    total_correct = 0
+    total_samples = 0
+    
+    model.eval()
+    with torch.no_grad():
+        for inputs, targets in data_loader:
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total_samples += targets.size(0)
+            total_correct += (predicted == targets).sum().item()
+            
+    accuracy = 100 * total_correct / total_samples
+    loss = criterion(outputs, targets).item()
+    
+    return accuracy, loss
 
 if __name__ == "__main__":
     from dataset_loader import *
@@ -54,65 +69,84 @@ if __name__ == "__main__":
 
     transform = transforms.Compose([transforms.PILToTensor()])
     
-    d_tr_loader = DataLoader(d_training2,batch_size=2,shuffle=True,num_workers=5)
+    batch_size = 10
+
+    d_tr_loader = DataLoader(d_training2,batch_size=batch_size,shuffle=True,num_workers=5)
     # d_te_loader =DataLoader(d_testing,batch_size=10,shuffle=True,num_workers=5)
 
-    
+
+    # Define the indices to split the dataset
+    dataset_size = len(d_tr_loader)
+    indices = list(range(dataset_size))
+    split = int(np.floor(0.8 * dataset_size))  # 80% training data, 20% testing data
+    np.random.shuffle(indices)  # shuffle the indices for randomness
+    train_indices, test_indices = indices[:split], indices[split:]
+
+    train_sampler = SubsetRandomSampler(train_indices)
+    test_sampler = SubsetRandomSampler(test_indices)
+
+    train_loader = DataLoader(d_tr_loader, batch_size=batch_size, sampler=train_sampler,num_workers=5)
+    test_loader = DataLoader(d_tr_loader, batch_size=batch_size, sampler=test_sampler,num_workers=5)
     
     model = torchvision.models.resnet18().cuda()
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
+    epochs = 5
+    k_folds = 5  # choose the number of folds
+    kf = KFold(n_splits=k_folds, shuffle=True, random_state=42)
+    for fold, (train_indices, val_indices) in enumerate(kf.split(train_loader.dataset)):
 
-    for epoch in range(5):  # loop over the dataset multiple times
-        model.train()
-        running_loss = 0.0
+        # Create new data loaders for this fold
+        train_subset = torch.utils.data.Subset(train_loader.dataset, train_indices)
+        train_loader_fold = DataLoader(train_subset, batch_size=train_loader.batch_size, shuffle=True)
+        
+        val_subset = torch.utils.data.Subset(train_loader.dataset, val_indices)
+        val_loader_fold = DataLoader(val_subset, batch_size=train_loader.batch_size)
 
-        f = 0
-        print("TRAINING STARTS NOW")
-        for i, data in enumerate(d_tr_loader):
-            # print(data)
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data   
-            inputs = inputs.cuda()
-            labels= labels.cuda()
-            # j=0
-            # while batch_size*j < len(inputs):
+        for epoch in range(epochs):  # loop over the dataset multiple times
+            model.train()
+            running_loss = 0.0
 
-            #     if batch_size*(j+1) > len(inputs):
-            #         frame = inputs[batch_size*j:].to(torch.float32).cuda()
-            #         label = labels[batch_size*j:].cuda()
-            #     else:
-            #         frame = inputs[batch_size*j:batch_size*(j+1)].to(torch.float32).cuda()
-            #         label = labels[batch_size*j:batch_size*(j+1)].cuda()
+            f = 0
+            print("TRAINING STARTS NOW")
+            for i, data in enumerate(train_loader_fold):
+                # print(data)
+                # get the inputs; data is a list of [inputs, labels]
+                inputs, labels = data   
+                inputs = inputs.cuda()
+                labels= labels.cuda()
                 
-            
-            # zero the parameter gradients
-            optimizer.zero_grad()
+                # zero the parameter gradients
+                optimizer.zero_grad()
 
-            # forward + backward + optimize
-            outputs = model(inputs)
-            # print( f"Prediction: {outputs} Label: {label}")
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+                # forward + backward + optimize
+                outputs = model(inputs)
+                # print( f"Prediction: {outputs} Label: {label}")
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
 
-            # print statistics
-            running_loss += loss.item()
-            if (i*10)%100 == 0:                
-                print(f'[epoch:{epoch + 1}, iter:{i :5d}, frame:{f}] loss: {running_loss/100}')
-                running_loss = 0.0
-            
+                # print statistics
+                running_loss += loss.item()
+                if (i*10)%100 == 0:                
+                    print(f'[epoch:{epoch + 1}, iter:{i :5d}, frame:{f}] loss: {running_loss/100}')
+                    running_loss = 0.0
+                
 
-            gc.collect()
-            torch.cuda.empty_cache()
-            # del frame
-            del inputs
-            del labels
+                gc.collect()
+                torch.cuda.empty_cache()
+                # del frame
+                del inputs
+                del labels
+        
+            torch.save(model.state_dict(), "./checkpoint")
+
+            accuracy,loss_eval = get_accuracy(model, val_loader_fold, criterion)
+            with open("checkpoints.txt", "a+") as f:
+                f.write(f" Epoch {epoch}\t Fold: {fold}\t Accuracy: {accuracy}\t Loss: {loss_eval}\n") 
     
-            torch.save(model.state_dict(), "./m1")
     
-
 
 print('Finished Training')
